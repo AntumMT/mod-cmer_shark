0087583c9b5c50d621ed06502f182d8f5d4a37fc
Jordan Irwin <antumdeluge@gmail.com>
2017-08-28 15:38:21 -0700

    Make shark meat edible

--- items.lua
+++ items.lua
@@ -20,12 +20,14 @@ end
 core.register_craftitem(':mobs:shark_meat_raw', {
 	description = S('Raw Shark Meat'),
 	inventory_image = 'mobs_shark_meat_raw.png',
+	on_use = core.item_eat(4),
 })
 core.register_alias('mobs:shark_meat', 'mobs:shark_meat_raw')
 
 core.register_craftitem(':mobs:shark_meat_cooked', {
 	description = S('Cooked Shark Meat'),
 	inventory_image = 'mobs_shark_meat_cooked.png',
+	on_use = core.item_eat(9),
 })
 
 core.register_craftitem(':mobs:shark_tooth', {
