2627068f7848742ff7042ad345086b11fade62ac
Jordan Irwin <antumdeluge@gmail.com>
2017-08-28 15:37:45 -0700

    Prepend ':' to registered craft items

--- items.lua
+++ items.lua
@@ -17,23 +17,23 @@ else
 end
 
 
-core.register_craftitem('mobs:shark_meat_raw', {
+core.register_craftitem(':mobs:shark_meat_raw', {
 	description = S('Raw Shark Meat'),
 	inventory_image = 'mobs_shark_meat_raw.png',
 })
 core.register_alias('mobs:shark_meat', 'mobs:shark_meat_raw')
 
-core.register_craftitem('mobs:shark_meat_cooked', {
+core.register_craftitem(':mobs:shark_meat_cooked', {
 	description = S('Cooked Shark Meat'),
 	inventory_image = 'mobs_shark_meat_cooked.png',
 })
 
-core.register_craftitem('mobs:shark_tooth', {
+core.register_craftitem(':mobs:shark_tooth', {
 	description = S('Shark Tooth'),
 	inventory_image = 'mobs_shark_tooth.png',
 })
 
-core.register_craftitem('mobs:shark_skin', {
+core.register_craftitem(':mobs:shark_skin', {
 	description = S('Shark Skin'),
 	inventory_image = 'mobs_shark_skin.png',
 })
