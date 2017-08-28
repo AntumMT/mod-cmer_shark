d2f212d72b0a6f70b0d81877391a1be872165bff
Jordan Irwin <antumdeluge@gmail.com>
2017-08-28 15:29:12 -0700

    Convert to 'mobs_redo' API

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -------------------------------------------------------------------------------
 -- Mob Framework Mod by Sapier
+-- Converted to 'mobs_redo' mod by Jordan Irwin (AntumDeluge)
 --
 -- You may copy, use, modify or do nearly anything except removing this
 -- copyright notice.
@@ -13,175 +14,156 @@
 --
 -- Contact sapier a t gmx net
 -------------------------------------------------------------------------------
---
+
+
 -- Boilerplate to support localized strings if intllib mod is installed.
 local S
-if (minetest.global_exists("intllib")) then
-  dofile(minetest.get_modpath("intllib").."/intllib.lua")
-  if (intllib.make_gettext_pair) then
-    -- New method using gettext.
-    S = intllib.make_gettext_pair(minetest.get_current_modname())
-  else
-    -- Old method using text files.
-    S = intllib.Getter(minetest.get_current_modname())
-  end
+if minetest.global_exists('intllib') then
+	dofile(minetest.get_modpath('intllib') .. '/intllib.lua')
+	if intllib.make_gettext_pair then
+		-- New method using gettext.
+		S = intllib.make_gettext_pair(minetest.get_current_modname())
+	else
+		-- Old method using text files.
+		S = intllib.Getter(minetest.get_current_modname())
+	end
 else
-  S = function ( s ) return s end
+	S = function ( s ) return s end
 end
 
-local version = "0.2.0"
+local version = '0.2.0'
+local variant = 'mobs_redo'
+local variant_version = '0.1'
+local version_full = version .. '-' .. variant .. '-' .. variant_version
 
-minetest.log("action","MOD: mob_shark loading ...")
+minetest.log('action','MOD: mob_shark loading ...')
 
-local selectionbox_shark = {-0.75, -0.5, -0.75, 0.75, 0.5, 0.75}
 
-local shark_groups = {
-						not_in_creative_inventory=1
-					}
+local mobname = 'mobs:shark'
+--local shark_collisionbox = {-0.75, -0.5, -0.75, 0.75, 0.5, 0.75}
+local shark_collisionbox = {-0.38, -0.25, -0.38, 0.38, 0.25, 0.38}
+local shark_drop = {
+	{name='animalmaterials:fish_shark', chance=1, min=3, max=3},
+	{name='animalmaterials:shark_tooth', chance=4, min=1, max=3},  -- FIXME: Original 'chance' value was 0.01
+	{name='animalmaterials:shark_skin', chance=4, min=1, max=1},  --FIXME: Original 'chance' value was 0.01
+}
 
-function shark_drop()
-	local result = {}
+mobs:register_mob(':' .. mobname, {
+	type = 'monster',
+	--passive = nil,
+	--docile_by_day = nil,
+	--attacks_monsters = nil,
+	--group_attack = nil,
+	owner_loyal = true,
+	attack_animals = true,
+	--specific_attack = nil,
+	hp_min = 10,  -- CHANGED (ORIGINAL: 5)
+	hp_max = 15,  -- CHANGED (ORIGINAL: 5)
+	--physical = nil,
+	collisionbox = shark_collisionbox,
+	visual = 'mesh',
+	visual_size = {x=0.5, y=0.5},  -- CHANGED (ORIGINAL: {x=1, y=1, z=1})
+	mesh = 'mob_shark.b3d',
+	textures = {
+		{'mob_shark_shark_mesh.png'},
+	},
+	--gotten_texture = nil,
+	--child_texture = nil,
+	--gotten_mesh = nil,
+	makes_footstep_sound = false,
+	--follow = nil,
+	view_range = 10,
+	--walk_chance = nil,
+	walk_velocity = 2,
+	run_velocity = 3,
+	--runaway = nil,
+	stepheight = 0.1,
+	jump = false,
+	jump_height = 0,
+	fly = true,
+	fly_in = 'default:water_source',  -- FIXME: Should be able to swim in 'default:water_flowing' as well
+	damage = 6,
+	--recovery_time = nil,
+	--knock_back = nil,
+	--immune_to = nil,
+	--blood_amount = nil,
+	--blood_texture = nil,
+	drops = shark_drop,
+	armor = 100,
+	--drawtype = nil,
+	rotate = 270,
+	water_damage = 0,
+	lava_damage = 10,
+	light_damage = 0,
+	--suffocation = nil,
+	--fall_damage = nil,
+	fall_speed = -9,
+	--fear_height = nil,
+	--on_die = nil,
+	--floats = nil,
+	--on_rightclick = nil,
+	--pathfinding = nil,
+	attack_type = 'dogfight',
+	--custom_attack = nil,
+	--double_melee_attack = nil,
+	--on_blast = nil,
+	reach = 2,
+	--sounds = nil,
+	animation = {
+		speed_normal = 24,
+		speed_run = 24,
+		stand_start = 1,
+		stand_end = 80,
+		walk_start = 80,
+		walk_end = 160,
+		run_start = 80,
+		run_end = 160,
+	},
+})
 
-	if math.random() < 0.01 then
-		table.insert(result,"animalmaterials:shark_tooth 3")
-	end
 
-	if math.random() < 0.01 then
-		table.insert(result,"animalmaterials:shark_skin 1")
-	end
+-- SPAWNING
+local min_light = tonumber(minetest.settings:get('mob_shark.min_light'))
+local max_light = tonumber(minetest.settings:get('mob_shark.max_light'))
+local active_object_count = tonumber(minetest.settings:get('mob_shark.active_object_count'))
+local min_height = tonumber(minetest.settings:get('mob_shark.min_height'))
+local max_height = tonumber(minetest.settings:get('mob_shark.max_height'))
+local day_toggle = minetest.settings:get_bool('mob_shark.day_toggle')
 
-	table.insert(result,"animalmaterials:fish_shark 3")
-
-	return result
+if min_light == nil then
+	min_light = 4
+end
+if max_light == nil then
+	max_light = 20
+end
+if chance == nil then
+	chance = 9000
+end
+if active_object_count == nil then
+	active_object_count = 1
+end
+if min_height == nil then
+	min_height = -30
+end
+if max_height == nil then
+	max_height = -3
 end
 
-local shark_prototype = {
-		name="shark",
-		modname="mob_shark",
+mobs:spawn({
+	name = mobname,
+	nodes = {'ethereal:seaweed'},
+	neighbors = {'default:water_source', 'default:water_flowing'},
+	min_light = min_light,
+	max_light = max_light,
+	interval = 60,
+	chance = 9000,  -- Override by setting 'mobs:shark_chance'
+	active_object_count = active_object_count,
+	min_height = min_height,
+	max_height = max_height,
+	day_toggle = day_toggle,
+})
 
-		factions = {
-			member = {
-				"animals",
-				"fish",
-				"sharks"
-				}
-			},
+mobs:register_egg(':' .. mobname, S('Shark'), 'mob_shark_shark_item.png', 0, false)
 
-		generic = {
-					description= S("Shark"),
-					base_health=5,
-					kill_result=shark_drop,
-					armor_groups= {
-						fleshy=20,
-					},
-					groups = shark_groups,
-					envid="deep_water",
-					population_density = 450,
-				},
-		movement =  {
-					default_gen="probab_mov_gen",
-					min_accel=0.1,
-					max_accel=0.3,
-					max_speed=0.8,
-					pattern="swim_pattern1",
-					canfly=true,
-					follow_speedup= { x=20,y=1.5,z=20 },
-				},
-		combat = {
-					angryness=1,
-					starts_attack=true,
-					sun_sensitive=false,
-					melee = {
-						maxdamage=5,
-						range=2,
-						speed=1,
-						},
-					distance 		= nil,
-					self_destruct 	= nil,
-					},
-		animation = {
-				swim = {
-					start_frame = 80,
-					end_frame   = 160,
-					},
-				stand = {
-					start_frame = 1,
-					end_frame   = 80,
-					},
-				},
-		states = {
-				{
-					name = "default",
-					movgen = "none",
-					chance = 0,
-					animation = "stand",
-					graphics_3d = {
-						visual = "mesh",
-						mesh = "mob_shark.b3d",
-						textures = {"mob_shark_shark_mesh.png"},
-						collisionbox = selectionbox_shark,
-						visual_size= {x=1,y=1,z=1},
-						},
-					graphics = {
-						sprite_scale={x=2,y=1},
-						sprite_div = {x=1,y=1},
-						visible_height = 1,
-						visible_width = 1,
-						},
-					typical_state_time = 30,
-				},
-				{
-					name = "swiming",
-					movgen = "probab_mov_gen",
-					chance = 0.9,
-					animation = "swim",
-					typical_state_time = 180,
-				},
-				{
-					name = "combat",
-					typical_state_time = 9999,
-					chance = 0.0,
-					animation = "swim",
-					movgen = "follow_mov_gen"
-				},
-				},
-		}
 
-local shark_name = shark_prototype.modname .. ":"  .. shark_prototype.name
-local shark_env = mobf_environment_by_name(shark_prototype.generic.envid)
-
-mobf_spawner_register("shark_spawner_1",shark_name,
-	{
-	spawnee = shark_name,
-	spawn_interval = 60,
-	spawn_inside = shark_env.media,
-	entities_around =
-		{
-			{ type="MAX",distance=1,threshold=0 },
-			{ type="MAX",entityname=shark_name,
-				distance=shark_prototype.generic.population_density,threshold=2 },
-		},
-
-	absolute_height =
-	{
-		min = -50,
-		max = -20
-	},
-
-	nodes_around =
-	{
-		{type="MIN",distance=2, name={ "default:water_flowing","default:water_source"},threshold=22},
-	},
-
-	-- set to empty to disable relative check
-	relative_height = {},
-
-	collisionbox = selectionbox_shark,
-
-	spawns_per_interval = 5
-	})
-
---register with animals mod
-minetest.log("action","\tadding mob "..shark_prototype.name)
-mobf_add_mob(shark_prototype)
-minetest.log("action","MOD: mob_shark mod version " .. version .. " loaded")
+minetest.log('action','MOD: mob_shark mod version ' .. version_full .. ' loaded')
